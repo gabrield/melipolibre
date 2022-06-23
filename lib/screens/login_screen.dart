1bb618391c46bc19fc02ed013c05fb81be67a244
Gabriel Duarte <gabs.ansf@gmail.com>
2022-06-23 15:34:03 -0300

    Tela de login agora com SingleChildScrollView + Padding

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -22,59 +22,62 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 80),
+          child: Container(
             padding: const EdgeInsets.symmetric(horizontal: 40),
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  SvgPicture.network(
-                    "https://upload.wikimedia.org/wikipedia/commons/4/45/Scaptotrigona_bipunctata_stingless_bee_abelha_sem_ferrao_tubuna.svg",
-                    width: 250,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                SvgPicture.network(
+                  "https://upload.wikimedia.org/wikipedia/commons/4/45/Scaptotrigona_bipunctata_stingless_bee_abelha_sem_ferrao_tubuna.svg",
+                  width: 250,
+                ),
+                TextField(
+                  controller: _userController,
+                  decoration: const InputDecoration(
+                    labelText: "Usuário",
                   ),
-                  TextField(
-                    controller: _userController,
-                    decoration: const InputDecoration(
-                      labelText: "Usuário",
-                    ),
-                  ),
-                  Row(
-                    children: [
-                      Flexible(
-                        child: TextField(
-                          controller: _passwordController,
-                          obscureText: _isHidden,
-                          decoration: InputDecoration(
-                            labelText: "Usuário",
-                            suffixIcon: IconButton(
-                              onPressed: _toggleHiddenPassword,
-                              icon: Icon(_isHidden
-                                  ? Icons.visibility_off
-                                  : Icons.visibility),
-                            ),
+                ),
+                Row(
+                  children: [
+                    Flexible(
+                      child: TextField(
+                        controller: _passwordController,
+                        obscureText: _isHidden,
+                        decoration: InputDecoration(
+                          labelText: "Usuário",
+                          suffixIcon: IconButton(
+                            onPressed: _toggleHiddenPassword,
+                            icon: Icon(_isHidden
+                                ? Icons.visibility_off
+                                : Icons.visibility),
                           ),
                         ),
                       ),
-                    ],
-                  ),
-                  const Divider(),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text('Entrar'),
-                  ),
-                  //const Divider(),
-                  TextButton(
-                    onPressed: () {},
-                    child: const Text('Esqueci a senha'),
-                  ),
-                ],
-              ),
+                    ),
+                  ],
+                ),
+                const Divider(),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Entrar'),
+                ),
+                //const Divider(),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text('Esqueci a senha'),
+                ),
+                const Divider(),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text('Cadastre-se'),
+                ),
+              ],
             ),
           ),
-        ],
+        ),
       ),
     );
   }
